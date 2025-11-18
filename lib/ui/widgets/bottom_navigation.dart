9ea16d1a3b8d49cc450c341f2275527727b29a70
nsperczynska <sperczynskanatalia@gmail.com>
2025-11-18 22:51:49 +0100

    Curved naviation bar

--- lib/ui/widgets/bottom_navigation.dart
+++ lib/ui/widgets/bottom_navigation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../view_models/home_view_model.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class AppBottomNavigationBar extends StatelessWidget{
     const AppBottomNavigationBar({super.key});
@@ -8,28 +9,18 @@ class AppBottomNavigationBar extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<HomeViewModel>();
-    return NavigationBar(
-      selectedIndex: viewModel.selectedIndex,
-      onDestinationSelected: (int index) {
+    return CurvedNavigationBar(
+      items:<Widget>[
+        const Icon(Icons.home_outlined),
+        const Icon(Icons.restaurant_menu_outlined),
+        const Icon(Icons.stacked_bar_chart_outlined),
+      ],
+      color: Theme.of(context).colorScheme.background,
+      index: viewModel.selectedIndex,
+      onTap: (int index) {
         context.read<HomeViewModel>().setIndex(index);
       },
-      destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.home_outlined),
-          selectedIcon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.restaurant_menu_outlined),
-          selectedIcon: Icon(Icons.restaurant_menu),
-          label: 'Meals',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.stacked_bar_chart_outlined),
-          selectedIcon: Icon(Icons.stacked_bar_chart),
-          label: 'Stats',
-        ),
-      ],
+
     );
   }
 }
